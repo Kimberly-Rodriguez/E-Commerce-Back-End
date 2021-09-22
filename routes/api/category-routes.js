eb3276256686bf5a6a911c01c9f1a93e2d802e83
kimberly Rodriguez <kimberlyrodriguez@kimberlys-iMac.attlocal.net>
2021-09-22 08:20:23 -0700

    created destroy by category/id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -45,7 +45,7 @@ router.put("/:id", async (req, res) => {
   try {
     const keyData = await Category.findByPk(req.body, {
       where: {
-        id: req.params.id,
+        id: req.params.id
       },
     });
     if (!keyData) {
@@ -58,8 +58,22 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const keyData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if(!keyData) {
+      res.status(404).json({message: 'No data found'});
+      return;
+    }
+    res.status(200).json({message:'Success!'});
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
