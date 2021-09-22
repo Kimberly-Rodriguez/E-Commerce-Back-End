4d7733e2d65fee7b34431e483671d323f329a139
kimberly Rodriguez <kimberlyrodriguez@kimberlys-iMac.attlocal.net>
2021-09-21 20:49:39 -0700

    created get one product for id via the product-routes.js file

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,7 +4,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products -- be sure to include its associated Category and Tag data
   try {
     const keyData = await Product.findAll({
@@ -17,9 +17,19 @@ router.get('/', (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  // find a single product by its `id` -- be sure to include its associated Category and Tag data
+  try {
+    const keyData = await Product.findByPk(req.params.id, {
+      include: [{model: Category}, {model: Tag, through: ProductTag, as: 'tags'}]
+    });
+    if(!keyData){
+      res.status(404).json({message: 'no data found'});
+    }
+    res.status(200).json(keyData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 // create new product
